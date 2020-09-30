ac2ba6c1ce23b7c142e437346ff8180721dde792
AkiraKotori <tnmying@outlook.com>
2020-09-30 18:27:33 +0800

    RPN

---
+++ part7/python/exercise1.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+class translator_RPN:
+    def __init__(self):
+        pass
+
+    def __call__(self,expr):
+        pass
